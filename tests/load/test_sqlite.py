05caf740f7ba4eda63ba1c2fd2ce8485d33fe903
Jay Lee <jl.listens+cloud9@gmail.com>
2016-07-14 13:57:38 +0000

    test add for SQLiteLoader

---
+++ tests/load/test_sqlite.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""Tests for :class:`SQLiteLoader`."""
+
+import decimal
+
+import pytest
+
+from sbscraper import escape
+from sbscraper.product import Product
+import sbscraper.load.sql
+
+
+@pytest.fixture
+def uri():
+    """Returns a URI to an SQLite database."""
+    return ':memory:'
+
+
+@pytest.fixture
+def loader(uri):
+    """Returns a :class:`SQLiteLoader` instance."""
+    return sbscraper.load.sql.SQLiteLoader(uri)
+
+
+def test_add(loader):
+    """Tests adding a :class:`Product`."""
+    product = Product(title='Apple',
+                      currency='SGD',
+                      price='1.23')
+    loader.add(product)
+
+    with loader.connection:
+        cursor = loader.connection.execute('SELECT details FROM products;')
+    row = cursor.fetchone()
+    assert escape.to_json(product) == row[0]
