0c1328d484d6c75b09fb5d1ad92bdd90abdbb338
Jay Lee <jl.listens+cloud9@gmail.com>
2016-07-14 23:09:49 +0000

    integrate pytest

--- setup.py
+++ setup.py
@@ -3,9 +3,26 @@ from setuptools import setup
 from sbscraper import __version__
 
 
-setup(name='sbscraper',
-      version=__version__,
-      packages=['sbscraper'],
-      entry_points = {
-            'console_scripts': ['sbscraper=sbscraper.executable:main'],
-      })
+setup(
+    name='sbscraper',
+    version=__version__,
+    packages=['sbscraper'],
+    install_requires=[
+        'beautifulsoup4',
+        'futures',
+        'lxml',
+        'requests',
+        'requests-futures'
+    ],
+    setup_requires=[
+        'pytest-runner'
+    ],
+    tests_require=[
+        'pytest'
+    ],
+    entry_points={
+        'console_scripts': [
+            'sbscraper=sbscraper.executable:main'
+        ]
+    }
+)
