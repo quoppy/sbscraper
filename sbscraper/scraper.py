85fff7db41cc3dea25ad924e1e46450377881537
Jay Lee <jl.listens+cloud9@gmail.com>
2016-07-14 23:00:58 +0000

    add docs, remove useless classes_from_label

--- sbscraper/scraper.py
+++ sbscraper/scraper.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Scrapers combine extractors, transformers and loaders."""
 
 from sbscraper.extract.lazada import LazadaExtractor
 from sbscraper.extract.redmart import RedMartExtractor
@@ -8,30 +9,39 @@ from sbscraper.transform.redmart import RedMartProductTransformer
 
 
 class Scraper(object):
+    """Default Scraper."""
 
     def __init__(self, extractor, transformer, loader):
+        """Constructor.
+
+        Arguments:
+            extractor (:class:`Extractor`): Extractor used to get data.
+            transformer (:class:`Transformer`): Converts data from an extractor
+                for a loader.
+            loader (:class:`Loader`): Puts data into a store.
+        """
         self.extractor = extractor
         self.transformer = transformer
         self.loader = loader
 
     def run(self):
+        """Runs the ETL process."""
         for item in self.extractor.extract():
             product = self.transformer.transform(item)
             self.loader.add(product)
 
 
 class VersionedScraper(Scraper):
+    """Scraper whose extractor and transformer work with a versioned API."""
 
     def __init__(self, extractor, transformer, loader):
         assert extractor.API_VERSION == transformer.API_VERSION
         super(VersionedScraper, self).__init__(extractor, transformer, loader)
 
 
-def classes_from_label(label):
-    labels = {
-        'lazada-sqlite': [Scraper, LazadaExtractor, LazadaProductTransformer,
-                          SQLiteLoader],
-        'redmart-sqlite': [VersionedScraper, RedMartExtractor,
-                           RedMartProductTransformer, SQLiteLoader]
-    }
-    return labels[label]
+LABELS = {
+    'lazada-sqlite': (Scraper, LazadaExtractor, LazadaProductTransformer,
+                      SQLiteLoader),
+    'redmart-sqlite': (VersionedScraper, RedMartExtractor,
+                       RedMartProductTransformer, SQLiteLoader)
+}
