e69d88be8ab191540a0b27c463e9acc1d96c2300
Jay Lee <jl.listens+cloud9@gmail.com>
2016-07-14 11:42:43 +0000

    add basic sqlite loader

---
+++ sbscraper/load/base.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""Loaders put data into some data store."""
+
+import abc
+
+
+class Loader(object):
+    """Base class for Loaders."""
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, uri):
+        """Constructor.
+
+        Arguments:
+            uri (str): URI to access for this Loader, eg. a connection string.
+        """
+        self.logger = logging.getLogger(__name__)
+        self.uri = uri
+
+    @abc.abstractmethod
+    def add(self, product):
+        """Adds a :class:`Product` to the data store.
+
+        Arguments:
+            product (:class:`Product`): The :class:`Product` instance to add.
+        """
+        pass
---
+++ sbscraper/load/sql.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""SQL-based Loaders."""
+
+import logging
+import os
+import sqlite3
+
+from sbscraper import escape
+from sbscraper.load import base
+
+
+_CREATE_PRODUCTS_TABLE_SQLITE = """
+CREATE TABLE products (
+    details text
+);
+"""
+
+_INSERT_PRODUCTS_TABLE_SQLITE = """
+INSERT INTO products
+VALUES (:details);
+"""
+
+
+class SQLiteLoader(base.Loader):
+    """Loads product data into an SQLite database."""
+
+    def __init__(self, uri):
+        super(SQLiteLoader, self).__init__(uri)
+        self.connection = sqlite3.connect(self.uri)
+        self._create_schema()
+
+    def add(self, product):
+        """Adds a :class:`Product` to an SQLite database.
+
+        Arguments:
+            product (:class:`Product`): The :class:`Product` instance to add.
+                Must be convertable to JSON.
+        """
+        as_text = escape.to_json(product)
+        try:
+            with self.connection:
+                self.connection.execute(_INSERT_PRODUCTS_TABLE_SQLITE,
+                                        {'details': as_text})
+        except sqlite3.Error as error:
+            self.logger.error("Error inserting row: %s", error)
+            raise
+
+    def _create_schema(self):
+        """Creates the schema to store product data."""
+        self.logger.debug("Creating table 'products'")
+        try:
+            self.connection.execute(_CREATE_PRODUCTS_TABLE_SQLITE)
+        except sqlite3.OperationalError:
+            self.logger.debug("Table 'products' already exists")
+        else:
+            self.logger.info("Created table 'products'")
