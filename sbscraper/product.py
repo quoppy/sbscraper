a39763ef81ff49eed3363d89e0325a8fef5f75f9
Jay Lee <jl.listens+cloud9@gmail.com>
2016-07-14 13:59:57 +0000

    add product model

---
+++ sbscraper/product.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Products are standard objects that Loaders add to the data store."""
+
+
+# For now Products are simple dictionaries.
+class Product(dict):
+    """Standardised product data."""
+    pass
