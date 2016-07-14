92fc3ddc4ad375a32f77c10d2a25449a13ef181f
Jay Lee <jl.listens+cloud9@gmail.com>
2016-07-14 20:44:33 +0000

    add docs

--- sbscraper/transform/base.py
+++ sbscraper/transform/base.py
@@ -6,31 +6,45 @@ import abc
 from sbscraper import product
 
 
-class Transformer(object):
+class ProductTransformer(object):
+    """Base class for Transformers that convert data to a :class:`Product`."""
 
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
     def get_currency(self, datum):
+        """Returns the currency for a product."""
         pass
 
     @abc.abstractmethod
     def get_current_price(self, datum):
+        """Returns the current price for a product."""
         pass
 
     @abc.abstractmethod
     def get_description(self, datum):
+        """Returns the description for a product."""
         pass
 
     @abc.abstractmethod
     def get_original_price(self, datum):
+        """Returns the original price for a product."""
         pass
 
     @abc.abstractmethod
     def get_title(self, datum):
+        """Returns the title for a product."""
         pass
 
     def transform(self, datum):
+        """Converts unit of data to a :class:`Product`.
+
+        Arguments:
+            datum: Unit of data to convert.
+
+        Returns:
+            :class:`Product`: Product instance.
+        """
         return product.Product(
             title=self.get_title(datum),
             description=self.get_description(datum),
